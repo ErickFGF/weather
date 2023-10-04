8dc40ba8c34a7ebdec8c4e48a22fce7e3a7faeb2
Cesar Ivann Llamas <ivllamoficial@gmail.com>
2023-10-04 01:14:51 -0600

    proyecto terminado

---
+++ src/Back/information.js
@@ -0,0 +1,15 @@
+const infoAPI = () => {
+    return {
+        registros: [
+            {id: 1,diaDeLaSemana: "lunes", clima: "soleado", temperaturaActual: 25, temperaturaMaxima: 28, temperaturaMinima: 18 },
+            {id: 2, diaDeLaSemana: "martes", clima: "nublado", temperaturaActual: 23, temperaturaMaxima: 25, temperaturaMinima: 17 },
+            {id: 3, diaDeLaSemana: "miércoles", clima: "lluvioso", temperaturaActual: 22, temperaturaMaxima: 24, temperaturaMinima: 19 },
+            {id: 4, diaDeLaSemana: "jueves", clima: "soleado", temperaturaActual: 24, temperaturaMaxima: 26, temperaturaMinima: 20 },
+            {id: 5, diaDeLaSemana: "viernes", clima: "ventoso", temperaturaActual: 21, temperaturaMaxima: 23, temperaturaMinima: 18 },
+            {id: 6, diaDeLaSemana: "sábado", clima: "nublado", temperaturaActual: 20, temperaturaMaxima: 22, temperaturaMinima: 18 },
+            {id: 7, diaDeLaSemana: "domingo", clima: "nevado", temperaturaActual: 5, temperaturaMaxima: 7, temperaturaMinima: -3 },
+        ]
+    }
+}
+
+export default infoAPI;
--- src/components/WeeklyBar/WeeklyBar.js
+++ src/components/WeeklyBar/WeeklyBar.js
@@ -1,31 +1,118 @@
 import './WeeklyBar.css';
+import infoAPI from '../../Back/information';
+import { useEffect, useState } from 'react';
+import {WiDaySunny, WiDayCloudy, WiDayRain, WiDayWindy, WiDaySnow} from 'react-icons/wi';
 
 function WeeklyBar({data, onClick}){
+    const [element, setElement] = useState(1);
+    const [infoData, setInfoData] = useState({
+        climaActual: null,
+        climaMax: null,
+        climaMin: null,
+        dia: null,
+    });
 
-    //const {day, cTemp, condition, types} = data;
+    const ClimaSign = ( clima ) => {
+        switch(clima){
+            case 'soleado':
+                return 'Sunny';
+            case 'nublado':
+                return 'Cloudy';
+            case 'lluvioso':
+                return 'Rain';
+            case 'ventoso':
+                return 'Windy';
+            case 'nevado': 
+                return 'Snow';
+            default: 
+                return 'No hay clima seleccionado'
+        }
+    }
+
+    const GetClickedElement = ( id ) => {
+        setElement( id );
+    }
+
+    const GetInfoClickedItem = () => {
+        const item = infoAPI().registros.find(item => element === item.id);
+        
+        if (item) {
+            setInfoData({
+                climaActual: item.temperaturaActual,
+                climaMax: item.temperaturaMaxima,
+                climaMin: item.temperaturaMinima,
+                dia: item.diaDeLaSemana,
+            });
+        }
+    }
+
+    const FormatDay = ( day ) => {
+        let first = day.charAt(0);
+        let rest = day.slice(1);
+        return first.toUpperCase() + rest;
+    }
+    
+    useEffect(() => {
+        GetInfoClickedItem();
+    }, [element]);
 
     return(
         <div className='father-container'>
-            <div className='global'>
-                <div>
-                    <div className='marker'>
-                        <div className='grades'>
-                            <h2>5<sub>°C</sub></h2>
+                <div className='global'>
+                    <div className='upper'>
+                        <div className='marker'>
+                            <div className='grades'>
+                                <h2>{infoData.climaActual}<sub>°C</sub></h2>
+                            </div>
+                            <div className='min-max'>
+                                <div className='min'>
+                                    <p className='text'>min</p>
+                                    <p className='number'>{infoData.climaMin}<sub>°C</sub></p>
+                                </div>
+                                <div className='max'>
+                                    <p className='text'>max</p>
+                                    <p className='number'>{infoData.climaMax}<sub>°C</sub></p>
+                                </div>
+                            </div>
                         </div>
-                        <div>
-                            <div>
-                                <p>min</p>
-                                <p>x</p>
-                            </div>
-                            <div>
-                                <p>max</p>
-                                <p>y</p>
-                            </div>
+                        <div className='days'>
+                            <h2>{FormatDay(infoData.dia)}</h2>
                         </div>
                     </div>
+                    <div className='down'>
+                        {infoAPI()['registros'].map(( item ) => (
+                            <div onClick={() => GetClickedElement(item.id)} className={element === item.id ? 'selected' : ''}>
+                                <p>{item.diaDeLaSemana.slice(0, 3)}</p>
+                                {ClimaSign( item.clima ) === 'Sunny' 
+                                    ? <WiDaySunny/>
+                                : 
+                                    null
+                                }
+                                {ClimaSign( item.clima ) === 'Cloudy'
+                                    ? <WiDayCloudy/>
+                                :
+                                    null
+                                }
+                                {ClimaSign( item.clima ) === 'Rain'
+                                    ? <WiDayRain/>
+                                :
+                                    null
+                                }
+                                {ClimaSign( item.clima ) === 'Windy'
+                                    ? <WiDayWindy/>
+                                :
+                                    null
+                                }
+                                {ClimaSign( item.clima ) === 'Snow'
+                                    ? <WiDaySnow/>
+                                :
+                                    null
+                                }
+                                <p>{item.temperaturaActual}°C</p>
+                            </div>
+                        ))}  
+                    </div>
                 </div>
-                <div></div>
-            </div>
         </div>
     )
 };
